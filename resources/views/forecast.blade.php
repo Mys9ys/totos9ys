ebdb621a3b818de384614aa75dec6ddc7d7a4e4b
user2 <admin@massagery24.ru>
2018-06-07 17:47:21 +0300

    новые прогнозы 2.3

--- resources/views/forecast.blade.php
+++ resources/views/forecast.blade.php
@@ -45,20 +45,31 @@
                 </div>
                 <div class="main_match_info_box">
                     <div class="main_match_info_title">Основное</div>
-                    <div class="match_result_wrap">
-                        <div class="win1_box"><span>П1</span><input name="dzen" type="radio" value="nedzen"></div>
-                        <div class="draw_box"><span>Н</span><input name="dzen" type="radio" value="nedzen"></div>
-                        <div class="win2_box"><span>П2</span><input name="dzen" type="radio" value="nedzen"></div>
-                    </div>
-                    <div class="goals_sum_wrap">
-                        <div class="goals_sum_title">
-                            <i class="sum-icon">∑ <i class="fa fa-futbol-o" aria-hidden="true"></i></i><span>Количество голов</span>
+                    <div class="match_result_wrap match_prop_wrap">
+                        <div class="match_prop_title left">
+                            <span>Результат матча</span>
+                            <b><i class="fa fa-newspaper-o" aria-hidden="true"></i></b>
+                        </div>
+                        <div class="match_prop_value right">
+                            <div class="win1_box win_box left"><span>П1</span><input name="dzen" type="radio" value="nedzen"></div>
+                            <div class="draw_box win_box left"><span>Н</span><input name="dzen" type="radio" value="nedzen"></div>
+                            <div class="win2_box win_box left"><span>П2</span><input name="dzen" type="radio" value="nedzen"></div>
                         </div>
                     </div>
-                    <div class="goals_margin_wrap">
-                        <div class="goals_margin_title">
-                            <i class="margin-icon">+/- <i class="fa fa-futbol-o" aria-hidden="true"></i></i><span>Разница голов</span>
-                            <p class="lamp"></p>
+                    <div class="goals_sum_wrap match_prop_wrap">
+                        <div class="goals_sum_title match_prop_title">
+                            <span>Количество голов</span>
+                            <b><i class="sum-icon">∑ <i class="fa fa-futbol-o" aria-hidden="true"></i></i></b>
+                        </div>
+                        <div class="match_prop_value right">
+                        </div>
+                    </div>
+                    <div class="goals_margin_wrap match_prop_wrap">
+                        <div class="goals_margin_title match_prop_title">
+                            <span>Разница голов</span>
+                            <b><i class="margin-icon">+/- <i class="fa fa-futbol-o" aria-hidden="true"></i></i></b>
+                        </div>
+                        <div class="match_prop_value right">
                         </div>
                     </div>
                 </div>
